11c42df8f32cae55a8bbb7903f83e4bf7b8efbad
Đỗ Hùng <dohung2ka3@gmail.com>
2024-04-30 13:01:09 +0700

    feat: use UVE_THREADPOOL to use all thread of cpu

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@
 // #      IMPORT NPM        #
 // ##########################
 import dotenv from "dotenv";
+import os from "os";
 dotenv.config();
 
 // ##########################
@@ -10,11 +11,19 @@ dotenv.config();
 import app from "./app.js";
 import connectDatabase from "./config/database.js";
 
+// Lấy số lượng CPU của hệ thống
+const numCPUs = os.cpus().length;
+
+// Thiết lập giá trị cho biến UV_THREADPOOL_SIZE
+// # Sử dụng hết các luồng của CPU để tăng hiệu suất
+process.env.UV_THREADPOOL_SIZE = numCPUs.toString();
+
 // Mongodb
 connectDatabase();
 
 // ##########################
 const PORT = process.env.PORT || 4000;
+
 app.listen(PORT, () => {
   console.log(`Server is working on http://localhost:${PORT}`);
 });
