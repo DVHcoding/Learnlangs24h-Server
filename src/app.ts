f8b0abfd3a2d2f1460a28d734b33dfc1c907390f
Đỗ Hùng <dohung2ka3@gmail.com>
2024-04-30 12:45:47 +0700

    feat: using compression to zip data

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import compression from "compression";
 
 // ##########################
 // #    IMPORT Components   #
@@ -27,6 +28,12 @@ app.use(
 app.use(bodyParser.json()); // dùng để consol.log được req.body gửi về từ phía client
 app.use(bodyParser.urlencoded({ extended: true })); // dùng để consol.log được req.body gửi về từ phía client
 app.use(cookieParser());
+app.use(
+  compression({
+    level: 6,
+    threshold: 100 * 1000, // 100 * 1000 byte = 100 kilobyte
+  })
+); // sử dụng để nén dữ liệu trả về giúp giảm kích thước json. Nếu cái nào lớn hơn 100 kilobyte thì mới nén
 
 // ##########################
 // #        ROUTES          #
