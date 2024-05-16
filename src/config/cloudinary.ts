20cd54b330133693ab088083b9e84e64a731d6a5
Đỗ Hùng <dohung2ka3@gmail.com>
2024-05-16 21:56:30 +0700

    fix(cloudinary): remove key and change to process.env

--- src/config/cloudinary.ts
+++ src/config/cloudinary.ts
@@ -1,10 +1,10 @@
-import { v2 as cloudinary } from "cloudinary";
+import { v2 as cloudinary } from 'cloudinary';
 
 // Cloudinary
 cloudinary.config({
-  cloud_name: "dvwdfsdkp",
-  api_key: "813336556963634",
-  api_secret: "tBgA6e2eZ2iO6RYAlVLDmWJey5Q",
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET,
 });
 
 export default cloudinary;
