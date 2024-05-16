44fd981da9052fc58c50cf40f474e5a6e93e16ee
Đỗ Hùng <dohung2ka3@gmail.com>
2024-05-16 21:36:32 +0700

    chore(jwtToken): comments somethings

--- src/utils/jwtToken.ts
+++ src/utils/jwtToken.ts
@@ -1,83 +1,69 @@
 // ##########################
 // #      IMPORT NPM        #
 // ##########################
-import { Response } from "express";
-import Users, { UserDocument } from "../models/userModel.js";
+import { Response } from 'express';
+import Users, { UserDocument } from '../models/userModel.js';
 
 // ##########################
 // #    IMPORT Components   #
 // ##########################
 
-export const sendToken = async (
-  user: UserDocument,
-  statusCode: number,
-  res: Response
-) => {
-  const token = user.getJWTToken();
-  const refreshToken = await user.generateRefreshToken();
+export const sendToken = async (user: UserDocument, statusCode: number, res: Response) => {
+    const token = user.getJWTToken();
+    const refreshToken = await user.generateRefreshToken();
 
-  // options for cookie
-  const millisecondsInOneDay = 24 * 60 * 60 * 1000;
-  const options = {
-    expires: new Date(
-      Date.now() + Number(process.env.COOKIE_EXPIRE) * millisecondsInOneDay
-    ),
-    httpOnly: true,
-    secure: true,
-  };
+    // options for cookie
+    const millisecondsInOneDay = 24 * 60 * 60 * 1000;
+    const options = {
+        expires: new Date(Date.now() + Number(process.env.COOKIE_EXPIRE) * millisecondsInOneDay),
+        httpOnly: true,
+        secure: true,
+    };
 
-  const optionsRefresh = {
-    expires: new Date(
-      Date.now() +
-        Number(process.env.COOKIE_REFRESH_EXPIRE) * millisecondsInOneDay
-    ),
-    httpOnly: true,
-    secure: true,
-  };
+    // tạo refresh token mới
+    const optionsRefresh = {
+        expires: new Date(Date.now() + Number(process.env.COOKIE_REFRESH_EXPIRE) * millisecondsInOneDay),
+        httpOnly: true,
+        secure: true,
+    };
 
-  res
-    .status(statusCode)
-    .cookie("token", token, options)
-    .cookie("refresh_token", refreshToken, optionsRefresh)
-    .json({
-      success: true,
-      user: {
-        _id: user._id,
-        username: user.username,
-        createAt: user.createdAt,
-        email: user.email,
-        photo: user.photo.url,
-      },
-    });
+    res.status(statusCode)
+        .cookie('token', token, options)
+        .cookie('refresh_token', refreshToken, optionsRefresh)
+        .json({
+            success: true,
+            user: {
+                _id: user._id,
+                username: user.username,
+                createAt: user.createdAt,
+                email: user.email,
+                photo: user.photo.url,
+            },
+        });
 };
 
-export const sendGoogleToken = async (
-  user: UserDocument,
-  statusCode: number,
-  res: Response
-) => {
-  const token = user.googleId;
+export const sendGoogleToken = async (user: UserDocument, statusCode: number, res: Response) => {
+    const token = user.googleId;
 
-  // options for cookie
-  const millisecondsInOneDay = 24 * 60 * 60 * 1000;
-  const options = {
-    expires: new Date(Date.now() + 7 * millisecondsInOneDay),
-    httpOnly: true,
-    secure: true,
-  };
+    // options for cookie
+    const millisecondsInOneDay = 24 * 60 * 60 * 1000;
+    const options = {
+        expires: new Date(Date.now() + 7 * millisecondsInOneDay),
+        httpOnly: true,
+        secure: true,
+    };
 
-  res
-    .status(statusCode)
-    .cookie("googleId", token, options)
-    .json({
-      success: true,
-      user: {
-        _id: user._id,
-        username: user.username,
-        createAt: user.createdAt,
-        email: user.email,
-        photo: user.photo.url,
-        role:user.roles 
-      },
-    });
+    res.status(statusCode)
+        .cookie('googleId', token, options)
+        .json({
+            success: true,
+            user: {
+                _id: user._id,
+                username: user.username,
+                createAt: user.createdAt,
+                email: user.email,
+                photo: user.photo.url,
+                role: user.roles,
+            },
+        });
 };
