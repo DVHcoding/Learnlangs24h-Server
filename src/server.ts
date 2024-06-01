211cc878d2b3005bbc3d25cfc6ab411012a5126a
Đỗ Hùng <dohung2ka3@gmail.com>
2024-06-01 17:40:16 +0700

    feat(server.ts): install socket.io

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@
 // #      IMPORT NPM        #
 // ##########################
 import dotenv from 'dotenv';
+import { Server } from 'socket.io';
+import { createServer } from 'http';
 import os from 'os';
 dotenv.config();
 
@@ -11,19 +13,43 @@ dotenv.config();
 import app from './app.js';
 import connectDatabase from './config/database.js';
 
+///////////////////////////////////////////////////////////
 // Lấy số lượng CPU của hệ thống
 const numCPUs = os.cpus().length;
-
 // Thiết lập giá trị cho biến UV_THREADPOOL_SIZE
 // # Sử dụng hết các luồng của CPU để tăng hiệu suất
 process.env.UV_THREADPOOL_SIZE = numCPUs.toString();
-
 // Mongodb
 connectDatabase();
 
-// ##########################
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: '*', // Cấu hình CORS nếu cần thiết
+        methods: ['GET', 'POST'],
+    },
+});
+
+// Lắng nghe sự kiện kết nối từ client
+io.on('connection', (socket) => {
+    console.log('New client connected');
+
+    // Xử lý các sự kiện từ client
+    socket.on('message', (data) => {
+        console.log('Message received:', data);
+        // Phát lại dữ liệu tới tất cả các client
+        io.emit('message', data);
+    });
+
+    // Xử lý sự kiện ngắt kết nối
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is working on http://localhost:${PORT}`);
 });
+////////////////////////////////////////////////////////////
