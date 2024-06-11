5aceaf7862692545d05161cb872edafa1fef3f87
Đỗ Hùng <dohung2ka3@gmail.com>
2024-06-11 15:47:38 +0700

    chore(server.ts): add credentials in cors

--- src/server.ts
+++ src/server.ts
@@ -28,11 +28,9 @@ connectDatabase();
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: [
-            "http://localhost:5173", 
-            "http://localhost:8080"
-        ], // Cấu hình CORS nếu cần thiết
-        methods: ['GET', 'POST'],
+        origin: ['http://localhost:5173', 'http://localhost:8080'], // Cấu hình CORS nếu cần thiết
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        credentials: true,
     },
 });
 
